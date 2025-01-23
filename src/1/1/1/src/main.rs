8eb3907ace70627cc3a52aee1c17191c3067c4f6
NiK <a.s7eve777@gmail.com>
2025-01-23 20:56:17 +0500

    Add: 1.1.5

--- src/1/1/1/src/main.rs
+++ src/1/1/1/src/main.rs
@@ -2,7 +2,7 @@
     Task:          1
     Level:         1.1
     Author:        https://github.com/NIKTO-IZ-NIOTKYDA
-    Task URL:      https://code.mu/ru/rust/tasker/stager/1/1/
+    Task URL:      https://code.mu/en/rust/tasker/stager/1/1/
     Repository:    https://github.com/NIKTO-IZ-NIOTKYDA/RustySolutions
 */
 
