275e953d8a1d27837644090c0695202f5f8891c7
NIK <141701604+NIKTO-IZ-NIOTKYDA@users.noreply.github.com>
2025-01-23 21:38:32 +0500

    Update README.md (#2)
    * Add .gitignore file
    
    * Add solution 1.1
    
    * Add: 1.2, 1.3
    
    * Add: documentation comments
    
    * Add: bash script for launching solutions
    
    * Add: .gitattributes file
    
    * Add: 1.1.4
    
    * Add: 1.1.5
    
    * Add: 1.1.6
    
    * Add: documentation comment
    
    * Add: 1.1.7
    
    * Fix: not a correct condition
    
    * Add: documentation comment
    
    * Add: Auto paste documentation comment
    
    * Add: 1.2.1
    
    * Add: Main Purpose to Readme.md

---
+++ src/1/2/1/src/main.rs
@@ -0,0 +1,12 @@
+/*
+    Task:          1
+    Level:         1.2
+    Author:        https://github.com/NIKTO-IZ-NIOTKYDA
+    Task URL:      https://code.mu/en/rust/tasker/stager/1/2/
+    Repository:    https://github.com/NIKTO-IZ-NIOTKYDA/RustySolutions
+*/
+
+fn main() {
+    // i8: Signed, range -128 to 127, can represent negative values.
+    // u8: Unsigned, range 0 to 255, can only represent non-negative values.
+}
