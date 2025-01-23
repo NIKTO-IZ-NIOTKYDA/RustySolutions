850c9f89b95332c17d10bf8ced1a546b5711048e
NiK <a.s7eve777@gmail.com>
2025-01-23 21:08:42 +0500

    Fix: not a correct condition

--- src/1/1/7/src/main.rs
+++ src/1/1/7/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let num: i16 = 5;
+    let num: i16 = 10;
 
-    if num >= 1 && num <= 10 {
+    if num > 1 && num < 10 {
         println!("{num} is between 1 and 10")
     } else {
         println!("{num} is not between 1 and 10")
