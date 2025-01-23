ea3f1f57a6efe7657074efa2c96465e128650d5b
NiK <a.s7eve777@gmail.com>
2025-01-23 21:08:08 +0500

    Add: 1.1.7

---
+++ src/1/1/7/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let num: i16 = 5;
+
+    if num >= 1 && num <= 10 {
+        println!("{num} is between 1 and 10")
+    } else {
+        println!("{num} is not between 1 and 10")
+    }
+}
