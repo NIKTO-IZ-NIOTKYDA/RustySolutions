9f32d161faf1f1615880db92e17dcc48932bb6c5
NiK <a.s7eve777@gmail.com>
2025-01-23 21:07:22 +0500

    Add: documentation comment

--- src/1/1/6/src/main.rs
+++ src/1/1/6/src/main.rs
@@ -1,3 +1,11 @@
+/*
+    Task:          6
+    Level:         1.1
+    Author:        https://github.com/NIKTO-IZ-NIOTKYDA
+    Task URL:      https://code.mu/en/rust/tasker/stager/1/1/
+    Repository:    https://github.com/NIKTO-IZ-NIOTKYDA/RustySolutions
+*/
+
 fn main() {
     let num: u16 = 123;
 
