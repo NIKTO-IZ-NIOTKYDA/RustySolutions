45ef4924366a4c1660b8d5b17c2618d8b6c3ec36
NiK <a.s7eve777@gmail.com>
2025-01-30 19:38:35 +0500

    Add: 1.2.4

---
+++ src/1/2/4/src/main.rs
@@ -0,0 +1,22 @@
+/*
+    Task:          4
+    Level:         1.2
+    Author:        https://github.com/NIKTO-IZ-NIOTKYDA
+    Task URL:      https://code.mu/en/rust/tasker/stager/1/2/
+    Repository:    https://github.com/NIKTO-IZ-NIOTKYDA/RustySolutions
+*/
+
+fn main() {
+    let num: i32 = 123;
+
+    let num_str = num.to_string();
+    let first_num = num_str.chars().next().unwrap();
+    let last_num = num_str.chars().last().unwrap();
+
+    println!("{}", char_to_i32(&first_num) + char_to_i32(&last_num));
+}
+
+fn char_to_i32(c: &char) -> i32 {
+    return *c as i32 - '0' as i32;
+}
+
