35b0ed911376a5d9d1f624fc8b7917b8219b790f
NiK <a.s7eve777@gmail.com>
2025-02-11 22:27:26 +0500

    Add: 1.2.5

---
+++ src/1/2/5/src/main.rs
@@ -0,0 +1,12 @@
+/*
+    Task:          5
+    Level:         1.2
+    Author:        https://github.com/NIKTO-IZ-NIOTKYDA
+    Task URL:      https://code.mu/en/rust/tasker/stager/1/2/
+    Repository:    https://github.com/NIKTO-IZ-NIOTKYDA/RustySolutions
+*/
+
+fn main() {
+    let num: i32 = 123;
+    println!("{}", num.to_string().len());
+}
